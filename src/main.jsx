56786829b83d9b8043a1f0f417fc592769a164c3
Xainab-U <139247678+Xainab-U@users.noreply.github.com>
2024-08-06 18:25:13 +0530

    Documentation Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+//To set up the React application and render the main App component into the root DOM node.
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(  //creates a root to the React application at the DOM element ID:root
   <React.StrictMode>
     <App />
   </React.StrictMode>,
